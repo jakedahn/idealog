13e0c7c8c02a2dd2792d6e8eb49c38ab15e82567
jakedahn <jake@markupisart.com>
2015-05-28 22:03:03 -0700

    updating settings paths

--- idealog/wsgi.py
+++ idealog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idealog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idealog.settings.base")
 
 application = get_wsgi_application()
