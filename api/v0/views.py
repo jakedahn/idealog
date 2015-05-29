d0c3b721fb9b32d200ecb2ad8807f8353c0c15e0
jakedahn <jake@markupisart.com>
2015-05-29 00:43:03 -0700

    cleaning up imports from uzis suggestion

--- api/v0/views.py
+++ api/v0/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from api.models import Idea
-from api.v0.serializers import IdeaSerializer
+from serializers import IdeaSerializer
 
 
 class IdeaList(APIView):
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from api.v1 import views
+import views
 
 urlpatterns = [
     url(r'^ideas', views.IdeaList.as_view()),
